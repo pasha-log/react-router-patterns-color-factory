498bff3f21c7b430f80614d658df26a75e51b494
pasha-log <pasha.log@gmail.com>
2023-01-17 10:21:08 -0800

    Updates

---
+++ precache-manifest.57f4b4adf011012c2ab549428107aa28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba38739df50061cfb59",
+    "url": "/react-router-patterns-color-factory/static/css/main.56622702.chunk.css"
+  },
+  {
+    "revision": "5ba38739df50061cfb59",
+    "url": "/react-router-patterns-color-factory/static/js/main.5ba38739.chunk.js"
+  },
+  {
+    "revision": "19e9041fe882a8d09af9",
+    "url": "/react-router-patterns-color-factory/static/css/1.295016d0.chunk.css"
+  },
+  {
+    "revision": "19e9041fe882a8d09af9",
+    "url": "/react-router-patterns-color-factory/static/js/1.19e9041f.chunk.js"
+  },
+  {
+    "revision": "6d2e62b539573e8fe32d",
+    "url": "/react-router-patterns-color-factory/static/js/runtime~main.6d2e62b5.js"
+  },
+  {
+    "revision": "c73de907421684761f09ffcd5329363e",
+    "url": "/react-router-patterns-color-factory/static/media/rainbow.c73de907.png"
+  },
+  {
+    "revision": "c9768d8359e0879195417f6e2412276f",
+    "url": "/react-router-patterns-color-factory/index.html"
+  }
+];
